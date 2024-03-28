25fd0541640f5cdb869b04283ea8a3aadb4e2ab8
Happy <89575098+Happy-del3@users.noreply.github.com>
2024-03-28 18:01:04 +0200

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,46 @@
+// Access and get user input
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String[] args){
+        // Declare variables  
+        int firstNumber, secondNumber, sum;
+    
+        // Instantiate --> Create Scanner object
+        Scanner input = new Scanner(System.in);
+
+        // Prompt for user to enter the firstNumber   
+        System.out.print("Please enter the first number: ");
+ 
+        // Read an integer from the keyboard, entered by the user and store it to the firstNumber
+        firstNumber = input.nextInt();
+ 
+        // Prompt for user to enter the secondNumber
+        System.out.print("Please enter the second number: ");
+ 
+        // Read an integer from the keyboard, entered by the user and store it to the secondNumber
+        secondNumber = input.nextInt();
+
+        // Call / Invoke determineSum()
+        sum = determineSum(firstNumber, secondNumber);
+   
+        // Call / Invoke displaySum()
+        displaySum(firstNumber, secondNumber, sum);
+
+    }
+    public static int determineSum(int firstNumber, int secondNumber){
+        // Determine sum of the two numbers
+        int sum;
+
+        return sum = firstNumber + secondNumber;
+
+    }
+    public static void displaySum(int firstNumber, int secondNumber, int sum){
+        // Display sum of the two numbers
+     
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is " + sum );
+   
+    }
+
+}
+     
